9db57e63b1b788a5a447adbd35d67a8cfdab08d6
Wills Bithrey <wills@willsbithrey.com>
2015-07-15 06:13:25 +0100

    Improve detection of FS9 vs FSX Links

--- MainForm.cs
+++ MainForm.cs
@@ -155,13 +155,24 @@ namespace Metacraft.FlightSimulation.WoaiDownloader
 					};
 					if (string.IsNullOrEmpty(pi.Country)) pi.Country = "N/A";
 					HtmlNodeCollection links = cells[5].SelectNodes("a");
-					if (links.Count == 2) {
-						pi.AvsimUrlFs9 = links[0].Attributes["href"].Value;
+                    List<HtmlAgilityPack.HtmlNode> avsimLinks = new List<HtmlAgilityPack.HtmlNode>();
+
+                    foreach (HtmlAgilityPack.HtmlNode link in links) {
+                        if (link.InnerText.ToLower().Trim() == "avsim") {
+                            avsimLinks.Add(link);
+                        }
+                    }
+
+                    if (avsimLinks.Count == 1) {
+                        pi.AvsimUrlFs9 = avsimLinks[0].Attributes["href"].Value;
 						pi.AvsimUrlFsx = pi.AvsimUrlFs9;
-					} else {
-						pi.AvsimUrlFs9 = links[0].Attributes["href"].Value;
-						pi.AvsimUrlFsx = links[2].Attributes["href"].Value;
-					}
+					} else if (avsimLinks.Count == 2) {
+                        pi.AvsimUrlFs9 = avsimLinks[0].Attributes["href"].Value;
+                        pi.AvsimUrlFsx = avsimLinks[1].Attributes["href"].Value;
+                    }
+                    else {
+                        MessageBox.Show(this, "Error finding AVSIM links for package: \"" + pi.Name + "\"", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
 					if (!mPackages[packageGroup.Name].ContainsKey(pi.Country)) mPackages[packageGroup.Name].Add(pi.Country, new List<PackageInfo>());
 					mPackages[packageGroup.Name][pi.Country].Add(pi);
 				}
