c0397466f356b94d978f1e77ecc8d30e32031f7e
Ross Carlson <ross@metacraft.com>
2015-07-16 19:43:00 -0400

    Fixed bug with simulator dropdown text.

--- MainForm.cs
+++ MainForm.cs
@@ -339,7 +339,7 @@ namespace Metacraft.FlightSimulation.WoaiDownloader
 				return;
 			}
 			AddMessage(string.Format("Fetching download link for {0} ...", mSelectedPackages[mCurrentPackageIndex].Name));
-			string downloadUri = (ddlSim.SelectedText == "FSX") ? mSelectedPackages[mCurrentPackageIndex].AvsimUrlFsx : mSelectedPackages[mCurrentPackageIndex].AvsimUrlFs9;
+			string downloadUri = (ddlSim.SelectedItem.ToString() == "FSX") ? mSelectedPackages[mCurrentPackageIndex].AvsimUrlFsx : mSelectedPackages[mCurrentPackageIndex].AvsimUrlFs9;
 			mPackageDownloadClient.DownloadStringAsync(new Uri(downloadUri));
 		}
 
